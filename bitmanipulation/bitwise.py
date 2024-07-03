a4028700c80af08e85c30625bfd988602cb58aff
Irvan Sian <irvansian@gmail.com>
2024-07-03 20:37:41 +0200

    add function(s) for setting k-th bit and similar operations

--- bitmanipulation/bitwise.py
+++ bitmanipulation/bitwise.py
@@ -6,6 +6,18 @@ def isPowerOfTwo(num):
     return (num & (num - 1) == 0) and num > 0
 
 
-def isBitSet(num, k):
+def isBitKSet(num, k):
     return num & (1 << k) != 0
+
+
+def toggleBitK(num, k):
+    return num ^ (1 << k)
+
+
+def setBitK(num, k):
+    return num | (1 << k)
+
+
+def unsetBitK(num, k):
+    return num & ~(1 << k)
 
