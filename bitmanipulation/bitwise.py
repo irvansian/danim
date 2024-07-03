779d557bf676c1047ed3143ebba216548956d76b
Irvan Sian <irvansian@gmail.com>
2024-07-03 20:50:16 +0200

    cool trick to toggle between two value

--- bitmanipulation/bitwise.py
+++ bitmanipulation/bitwise.py
@@ -21,3 +21,16 @@ def setBitK(num, k):
 def unsetBitK(num, k):
     return num & ~(1 << k)
 
+
+def toggleTwoValue(num):
+    """
+    if num == a:
+        return b
+    elif num == b:
+        return a
+    :param num:
+    :return:
+    """
+    a = 10
+    b = 20
+    return a ^ b ^ num
